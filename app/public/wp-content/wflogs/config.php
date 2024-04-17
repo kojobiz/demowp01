46ad425d78ab7d1f0240730d5298f8477f82fb72
小城 <kojolumi@gamil.com>
2024-04-17 10:26:40 +0900

    plugin theme

---
+++ app/public/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1713921814;s:7:"authKey";s:64:"*JZ~f8n^m&zu0Fkh*ccV]]*g0b:LlQ`BVb<VV.-E5GNh/Ap{gGPg5(6{V03!{_Ur";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:46;}
